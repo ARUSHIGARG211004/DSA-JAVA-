0dfdd3d4783740bf3defbb7f9395cbc582fc3f7d
ARUSHIGARG211004 <121043298+ARUSHIGARG211004@users.noreply.github.com>
2024-02-16 02:42:26 +0530

    Add files via upload

---
+++ LinkedList(search a key).java
@@ -0,0 +1,89 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+        
+        public Node(int data){
+            this.data=data;
+            this.next=null;        
+            
+         }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+    
+    public void addFirst(int data){
+        //step 1- Create a newNode;
+        Node newNode= new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        //step 2- newNode.next=head
+        newNode.next=head;//link
+        //head=newNode
+        head=newNode;
+    }
+    public void addLast(int data){
+        Node newNode= new Node(data);
+        size++;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+        
+    }
+    public void add(int index, int data){
+        if(index==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode=new Node(data);
+        size++;
+        Node temp= head;
+        int i=0;
+        if(i<index-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+        
+        
+    }
+    public void print(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+   
+    public int itrSearch(int key){
+        Node temp=head;
+        int i=0;
+        while(temp!=null){
+            if(temp.data==key){
+                return i;
+            }
+            temp=temp.next;
+            i++;}
+            return -1;
+    }
+    public static void main(String[]args){
+        LinkedList ll=new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.add(2,9);
+        ll.print();
+        System.out.println(ll.itrSearch(3));
+        System.out.println(ll.itrSearch(10));
+    }
+}
