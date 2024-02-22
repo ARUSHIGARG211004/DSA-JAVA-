bd209663d37e3410c8eb2e3d3d1807c9e61dc989
ARUSHIGARG211004 <121043298+ARUSHIGARG211004@users.noreply.github.com>
2024-02-23 02:03:11 +0530

    Add files via upload

---
+++ StackB(reverse a stack using stack).java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class StackB{
+    public static String ReverseString(String str){
+        Stack <Character>s= new Stack<>();
+        int idx=0;
+        while(idx<str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder result=new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr= s.pop();
+            result.append(curr);
+            
+        }
+        return result.toString();
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String str= sc.nextLine();
+        String result= ReverseString(str);
+        System.out.println(result);
+    }
+}
