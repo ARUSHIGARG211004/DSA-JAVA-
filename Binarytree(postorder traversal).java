6373098476391601e54d2611ea7b2544d9d2228f
ARUSHIGARG211004 <121043298+ARUSHIGARG211004@users.noreply.github.com>
2024-04-11 02:40:46 +0530

    Add files via upload

---
+++ Binarytree(postorder traversal).java
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Binarytree
+{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class binarytree{
+        static int idx=-1;
+        public static Node buildtree(int node[]){
+        idx++;
+        if(node[idx]==-1){
+            return null;
+        }
+        Node newNode=new Node(node[idx]);
+        newNode.left=buildtree(node);
+        newNode.right=buildtree(node);
+        return newNode;
+        }
+    
+     public static void postorder(Node root){
+         if(root==null){
+            
+             return;
+         }
+     
+         postorder(root.left);
+         postorder(root.right);
+         System.out.print(root.data+" ");
+     }
+    }
+	public static void main(String[] args) {
+	int node[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+	binarytree tree= new binarytree();
+	Node root=tree.buildtree(node);
+	
+    tree.postorder(root);
+	
+	}
+}
